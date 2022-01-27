be28a27b8ffd57ccd2f9922fbbe19b960a9fbd05
modpage1512 <pacheco1512@gmail.com>
2022-01-27 00:31:03 -0400

    ingresado el navbar y el titulo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,46 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './HeaderStyles';
 
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{display: "flex", alignItems: "center", color: "white", marginBottom: "20"}}>
+          <DiCssdeck size="3rem"/><Span>Portafolio</Span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Proyectos</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#tech">
+          <NavLink>Tecnologias</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>Sobre mi</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <Div3>
+      <SocialIcons href="http://github.com">
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="http://likedin.com">
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href="http://instagram.com">
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
+  </Container>
 );
 
 export default Header;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Bienvenido a <br/>
+        Mi portafolio personal
+      </SectionTitle>
+      <SectionText>
+        Programador full-stack, la imaginación es el limite y el limite te lo impones tu mismo.
+      </SectionText>
+      <Button onClick={() => window.location="https://google.com"}>Saber más</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
